cce98a5347361729fe80764c5cdd933c87da087f
Jumana Dakka <jumanadakka@gmail.com>
2018-06-16 22:14:25 -0400

    iteration on 4D ESMACS/TIES

---
+++ e-science18/esmacs.py
@@ -0,0 +1,15 @@
+# creating a customized ESMACS protocol instance
+import htbac.protocols.Esmacs
+esmacs_1 = Protocol(system = 'e255k')
+sim = Simulation(name = 'minimization')
+        ... # define simulation conditions 
+sim.ensemble('replica', range(25))
+# add simulation configurations to protocol
+Esmacs.step0 = sim
+esmacs_1.append(Esmacs.step0)
+# assign resources, append protocol to Runner 
+runner = Runner(resource = 'ncsa.blue_waters',
+                walltime = 60) 
+runner.add_protocol(esmacs_1)
+# launch application
+runner.run()
