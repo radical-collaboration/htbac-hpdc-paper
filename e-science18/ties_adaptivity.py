9242be73311ff95d0d5ad10764a891079e46f9f6
Jumana Dakka <jumanadakka@gmail.com>
2018-06-17 19:42:28 -0400

    modified Rfe to TIES in snippet

--- e-science18/ties_adaptivity.py
+++ e-science18/ties_adaptivity.py
@@ -1,5 +1,5 @@
 # creating a customized adaptive TIES protocol 
-import htbac.protocols.Rfe
+import htbac.protocols.TIES
 ties_1 = Protocol(system = 'e255k')
         ...
 sim = Simulation(name = 'production MD phase 1')
@@ -7,8 +7,8 @@ sim = Simulation(name = 'production MD phase 1')
 sim.ensemble('replica', range(5))
 sim.ensemble('lambda' , range(3))
 
-Rfe.step3 = sim
-ties_1.append(Rfe.step3)
+TIES.step3 = sim
+ties_1.append(TIES.step3)
 
 runner = Runner() 
 runner.add_protocol(protocol = ties_1)
@@ -24,7 +24,7 @@ sim1 = Simulation(name = 'production MD phase 2')
 sim1.ensemble('replica', range(5))
 sim1.ensemble('additional lambdas', requested_windows)
 
-Rfe.step4 = sim1
-ties_1.append(Rfe.step4)
+TIES.step4 = sim1
+ties_1.append(TIES.step4)
 runner.run(protocol = ties_1, terminate = True)
 
